364b71629dcfc9d23e960cfce09f8b6e9c7b94e6
bubbashub1 <bubbashub1@gmail.com>
2026-09-16 20:21:14 +0100

    Load Stage 2 account settings module

--- myhub/myhub.php
+++ myhub/myhub.php
@@ -37,4 +37,5 @@ function bubbahub_myhub_shortcode(){return '<div class="bh-myhub-login"><h2>Load
 require_once BUBBAHUB_MYHUB_PATH . 'myhub-groups.php';
 if ( file_exists( BUBBAHUB_MYHUB_PATH . 'myhub-v2.php' ) ) require_once BUBBAHUB_MYHUB_PATH . 'myhub-v2.php';
 if ( file_exists( BUBBAHUB_MYHUB_PATH . 'profile-settings.php' ) ) require_once BUBBAHUB_MYHUB_PATH . 'profile-settings.php';
+if ( file_exists( BUBBAHUB_MYHUB_PATH . 'account-settings-stage2.php' ) ) require_once BUBBAHUB_MYHUB_PATH . 'account-settings-stage2.php';
 if ( file_exists( BUBBAHUB_MYHUB_PATH . 'myhub-account-link.php' ) ) require_once BUBBAHUB_MYHUB_PATH . 'myhub-account-link.php';

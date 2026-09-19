21071ca79ed7abeec3ba73d40e8b2b030bdcc53c
bubbashub1 <bubbashub1@gmail.com>
2026-09-19 17:20:55 +0100

    Enqueue AJAX Calendar script

--- myhub/myhub-weekly-planner-v2-assets.php
+++ myhub/myhub-weekly-planner-v2-assets.php
@@ -4,4 +4,6 @@ add_action( 'wp_enqueue_scripts', function() {
     if ( ! is_user_logged_in() ) return;
     wp_register_style( 'bubbahub-myhub-weekly-planner-v2', BUBBAHUB_MYHUB_URL . 'myhub-weekly-planner-v2.css', array(), BUBBAHUB_MYHUB_VERSION );
     wp_enqueue_style( 'bubbahub-myhub-weekly-planner-v2' );
+    wp_register_script( 'bubbahub-myhub-weekly-planner-v2', BUBBAHUB_MYHUB_URL . 'myhub-weekly-planner-v2.js', array( 'jquery' ), BUBBAHUB_MYHUB_VERSION, true );
+    wp_enqueue_script( 'bubbahub-myhub-weekly-planner-v2' );
 } );

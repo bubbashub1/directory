dfd5a383deafc81989a5f8aff18743dfd6b12629
bubbashub1 <bubbashub1@gmail.com>
2026-09-19 15:57:19 +0100

    Fix remaining planner hour-line PHP syntax

--- myhub/myhub-weekly-planner-v2.php
+++ myhub/myhub-weekly-planner-v2.php
@@ -427,7 +427,7 @@ function bubbahub_myhub_weekly_planner_v2_shortcode() {
         <div class="bh-timetable-body" style="--bh-grid-hours:<?php echo esc_attr($grid_hours); ?>;--bh-hour-px:60px;">
           <div class="bh-timetable-times"><?php for($h=$grid_start;$h<=$grid_end;$h+=60): ?><span style="top:calc(<?php echo esc_attr(($h-$grid_start)/60); ?> * var(--bh-hour-px));"><?php echo esc_html(sprintf('%02d:00',floor($h/60))); ?></span><?php endfor; ?></div>
           <?php foreach($display_days as $day): $day_lane_count=0; foreach($week_by[$day] as $day_item) $day_lane_count=max($day_lane_count,(int)($day_item['lane_count']??1)); ?><div class="bh-timetable-column<?php echo $day_lane_count>2?' bh-timetable-column-many':''; ?>" data-day="<?php echo esc_attr($day); ?>" data-lane-count="<?php echo esc_attr($day_lane_count); ?>">
-            <?php for($h=$grid_start;$h<$grid_end;$h+=60): ?><div class="bh-timetable-hour-line" style="top:<?php echo esc_attr((($h-$grid_start)/60)*var(--bh-hour-px)); ?>px;"></div><?php endfor; ?>
+            <?php for($h=$grid_start;$h<$grid_end;$h+=60): ?><div class="bh-timetable-hour-line" style="top:calc(<?php echo esc_attr(($h-$grid_start)/60); ?> * var(--bh-hour-px));"></div><?php endfor; ?>
             <?php if(!empty($week_by[$day])): foreach($week_by[$day] as $item):
               $start_ts=strtotime($item['date'].' '.$item['start']);$end_ts=$item['end']?strtotime($item['date'].' '.$item['end']):$start_ts+3600;if($end_ts<$start_ts)$end_ts=strtotime('+1 day',$end_ts);
               $start_m=(int)wp_date('H',$start_ts)*60+(int)wp_date('i',$start_ts);$end_m=(int)wp_date('H',$end_ts)*60+(int)wp_date('i',$end_ts);
